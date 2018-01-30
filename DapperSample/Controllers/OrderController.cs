1f8df9e858a80d9b5f45a9dac35cd86f28884b8f
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-01-30 20:39:00 -0200

    - Add Order

---
+++ DapperSample/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using DapperSample.Models;
+using DapperSample.Models.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace DapperSample.Controllers
+{
+    [EnableCors(origins: "http://localhost:55150", headers: "*", methods: "*")]
+    [RoutePrefix("Order")]
+    public class OrderController : ApiController
+    {
+        private OrderRepository Repository { get; set; } = new OrderRepository();
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok<List<Order>>(this.Repository.GetOrders());
+        }
+    }
+}
---
+++ DapperSample/Models/Repository/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DapperSample.Models.Repository
+{
+    public class OrderRepository
+    {
+        public string ConnectionString = ConfigurationManager.ConnectionStrings["conn"].ToString();
+
+        public List<Order> GetOrders()
+        {
+            List<Order> orders = new List<Order>();
+
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                var result = sqlConnection.Query<Order>("Select * from Orders");
+
+                foreach (Order order in result)
+                    orders.Add(order);
+            }
+
+            return orders;
+        }
+    }
+}
